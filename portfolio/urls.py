57c9f494579aadf552cc43e1f3c2da3fc5fe7f6e
Francis Tsang <frtsang40@gmail.com>
2020-06-12 16:33:01 -0700

    changing url nameing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='portfolio'),
     path('contact/', views.contact, name='contact'),
     path('greet/<str:name>/', views.greet_by_name),
 ]
